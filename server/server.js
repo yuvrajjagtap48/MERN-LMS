72ee24b5bacd6f14d52ce38c2e99761d1fcad637
Yuvraj Jagtap <140615246+yuvrajjagtap48@users.noreply.github.com>
2025-08-04 23:53:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT"], 
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
