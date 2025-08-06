998188e5f1b4e9c30d4241428822a53aad10dcb9
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-08-06 23:18:04 +0530

    add auth

--- client/src/components/route-guard/index.jsx
+++ client/src/components/route-guard/index.jsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { Fragment } from "react";
 
+
 function RouteGuard({ authenticated, user, element }) {
   const location = useLocation();
 
