68f45c7a32c4ab7196af7bbefcbbe782aaf65aba
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-08-06 19:41:54 +0530

    UPDATE

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from './context/auth-context'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/auth-context/index.jsx";
+import InstructorProvider from "./context/instructor-context/index.jsx";
+import StudentProvider from "./context/student-context/index.jsx";
 
-createRoot(document.getElementById('root')).render(
-
-  <BrowserRouter> 
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <InstructorProvider>
+        <StudentProvider>
+          <App />
+        </StudentProvider>
+      </InstructorProvider>
+    </AuthProvider>
   </BrowserRouter>
-
-)
+);
