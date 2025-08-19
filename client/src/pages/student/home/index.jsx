fb242224187a77d88fffec02b304d4e31e90d15c
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-08-20 00:04:42 +0530

    update

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,5 +1,5 @@
 import { courseCategories } from "@/config";
-import banner from "../../../../public/banner-img.png";
+import banner from "/banner-img.png";
 import { Button } from "@/components/ui/button";
 import { useContext, useEffect } from "react";
 import { StudentContext } from "@/context/student-context";
